e92dbf6fb11379f425c99a06fa628699e0c571fe
Julio Fuertes <juliofuertestarin@gmail.com>
2024-05-05 12:32:03 +0200

    initial

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Contact() {
+    return (
+        <main className="flex min-h-screen flex-col">
+            <h1>Contact</h1>
+        </main>
+    )
+}
---
+++ src/app/projects/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Projects() {
+    return (
+        <main className="flex min-h-screen flex-col">
+            <h1>Main page</h1>
+        </main>
+    )
+}
